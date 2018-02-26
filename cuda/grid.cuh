6a61708da4cb2e00d38347db1a95203a3d08c724
ToruNiina <niina.toru.68u@gmail.com>
2018-02-26 16:07:12 +0900

    parallelize adjacent index generation

--- cuda/grid.cuh
+++ cuda/grid.cuh
@@ -1,13 +1,75 @@
 #ifndef LJ_GRID_H
 #define LJ_GRID_H
-#include <cuda/array.h>
+#include <cuda/array.cuh>
 #include <cuda/particle.cuh>
 #include <cuda/boundary_condition.cuh>
 #include <thrust/device_vector.h>
-#include <thrust/zip_iterator.h>
+#include <thrust/iterator/zip_iterator.h>
+#include <thrust/sort.h>
+#include <thrust/gather.h>
 
 namespace lj
 {
+namespace detail
+{
+
+struct make_adjacents
+{
+    const std::size_t Nx, Ny, Nz;
+
+    make_adjacents(const std::size_t x, const std::size_t y, const std::size_t z)
+        : Nx(x), Ny(y), Nz(z)
+    {}
+
+    __host__ __device__
+    ::lj::array<std::size_t, 27> operator()(const std::size_t idx) const noexcept
+    {
+        const std::size_t x = idx % Nx;
+        const std::size_t y = (idx / Nx) % Ny;
+        const std::size_t z = (idx / Nx / Ny);
+
+        ::lj::array<std::size_t, 27> adjacents;
+        adjacents[ 0] = calc_index((x  == 0 ? Nx-1 : x-1), (y  == 0 ? Ny-1 : y-1), (z==0 ? Nz-1 : z-1));
+        adjacents[ 1] = calc_index( x,                     (y  == 0 ? Ny-1 : y-1), (z==0 ? Nz-1 : z-1));
+        adjacents[ 2] = calc_index((x+1==Nx ?    0 : x+1), (y  == 0 ? Ny-1 : y-1), (z==0 ? Nz-1 : z-1));
+        adjacents[ 3] = calc_index((x  == 0 ? Nx-1 : x-1), y,                      (z==0 ? Nz-1 : z-1));
+        adjacents[ 4] = calc_index( x,                     y,                      (z==0 ? Nz-1 : z-1));
+        adjacents[ 5] = calc_index((x+1==Nx ?    0 : x+1), y,                      (z==0 ? Nz-1 : z-1));
+        adjacents[ 6] = calc_index((x  == 0 ? Nx-1 : x-1), (y+1==Ny ?    0 : y+1), (z==0 ? Nz-1 : z-1));
+        adjacents[ 7] = calc_index( x,                     (y+1==Ny ?    0 : y+1), (z==0 ? Nz-1 : z-1));
+        adjacents[ 8] = calc_index((x+1==Nx ?    0 : x+1), (y+1==Ny ?    0 : y+1), (z==0 ? Nz-1 : z-1));
+
+        adjacents[ 9] = calc_index((x  == 0 ? Nx-1 : x-1), (y  == 0 ? Ny-1 : y-1), z);
+        adjacents[10] = calc_index( x,                     (y  == 0 ? Ny-1 : y-1), z);
+        adjacents[11] = calc_index((x+1==Nx ?    0 : x+1), (y  == 0 ? Ny-1 : y-1), z);
+        adjacents[12] = calc_index((x  == 0 ? Nx-1 : x-1), y,                      z);
+        adjacents[13] = calc_index( x,                     y,                      z);
+        adjacents[14] = calc_index((x+1==Nx ?    0 : x+1), y,                      z);
+        adjacents[15] = calc_index((x  == 0 ? Nx-1 : x-1), (y+1==Ny ?    0 : y+1), z);
+        adjacents[16] = calc_index( x,                     (y+1==Ny ?    0 : y+1), z);
+        adjacents[17] = calc_index((x+1==Nx ?    0 : x+1), (y+1==Ny ?    0 : y+1), z);
+
+        adjacents[18] = calc_index((x  == 0 ? Nx-1 : x-1), (y  == 0 ? Ny-1 : y-1), (z+1==Nz ? 0 : z+1));
+        adjacents[19] = calc_index( x,                     (y  == 0 ? Ny-1 : y-1), (z+1==Nz ? 0 : z+1));
+        adjacents[20] = calc_index((x+1==Nx ?    0 : x+1), (y  == 0 ? Ny-1 : y-1), (z+1==Nz ? 0 : z+1));
+        adjacents[21] = calc_index((x  == 0 ? Nx-1 : x-1), y,                      (z+1==Nz ? 0 : z+1));
+        adjacents[22] = calc_index( x,                     y,                      (z+1==Nz ? 0 : z+1));
+        adjacents[23] = calc_index((x+1==Nx ?    0 : x+1), y,                      (z+1==Nz ? 0 : z+1));
+        adjacents[24] = calc_index((x  == 0 ? Nx-1 : x-1), (y+1==Ny ?    0 : y+1), (z+1==Nz ? 0 : z+1));
+        adjacents[25] = calc_index( x,                     (y+1==Ny ?    0 : y+1), (z+1==Nz ? 0 : z+1));
+        adjacents[26] = calc_index((x+1==Nx ?    0 : x+1), (y+1==Ny ?    0 : y+1), (z+1==Nz ? 0 : z+1));
+
+        return adjacents;
+    }
+
+    __host__ __device__
+    std::size_t calc_index(
+            const std::size_t i, const std::size_t j, const std::size_t k) const
+    {
+        return k * Ny * Nx + j * Nx + i;
+    }
+};
+} // detail
 
 struct grid
 {
@@ -25,16 +87,10 @@ struct grid
         this->adjs.resize(Nx * Ny * Nz);
         this->cell.resize(Nx * Ny * Nz);
 
-        for(std::size_t k=0; k<Nz; ++k)
-        {
-            for(std::size_t j=0; j<Ny; ++j)
-            {
-                for(std::size_t i=0; i<Nx; ++i)
-                {
-                    adjs[calc_index(i, j, k)] = make_adjacents(i, j, k);
-                }
-            }
-        }
+        thrust::transform(
+                thrust::make_counting_iterator<std::size_t>(0),
+                thrust::make_counting_iterator<std::size_t>(Nx * Ny * Nz),
+                this->adjs.begin(), detail::make_adjacents(Nx, Ny, Nz));
     }
 
     __host__
@@ -93,44 +149,6 @@ struct grid
     thrust::device_vector<std::size_t> cell; // number of particles in each cell
 
   private:
-
-    __host__
-    array<std::size_t, 27> make_adjacents(
-            const std::size_t x, const std::size_t y, const std::size_t z) const
-    {
-        array<std::size_t, 27> adjacents;
-        adjacents[ 0] = calc_index((x  == 0 ? Nx : x-1), (y == 0 ? Ny : y-1), (z==0 ? Nz : z-1));
-        adjacents[ 1] = calc_index( x,                   (y == 0 ? Ny : y-1), (z==0 ? Nz : z-1));
-        adjacents[ 2] = calc_index((x+1==Nx ?  0 : x+1), (y == 0 ? Ny : y-1), (z==0 ? Nz : z-1));
-        adjacents[ 3] = calc_index((x  == 0 ? Nx : x-1), y,                   (z==0 ? Nz : z-1));
-        adjacents[ 4] = calc_index( x,                   y,                   (z==0 ? Nz : z-1));
-        adjacents[ 5] = calc_index((x+1==Nx ?  0 : x+1), y,                   (z==0 ? Nz : z-1));
-        adjacents[ 6] = calc_index((x  == 0 ? Nx : x-1), (y+1==Ny ? 0 : y+1), (z==0 ? Nz : z-1));
-        adjacents[ 7] = calc_index( x,                   (y+1==Ny ? 0 : y+1), (z==0 ? Nz : z-1));
-        adjacents[ 8] = calc_index((x+1==Nx ?  0 : x+1), (y+1==Ny ? 0 : y+1), (z==0 ? Nz : z-1));
-
-        adjacents[ 9] = calc_index((x  == 0 ? Nx : x-1), (y == 0 ? Ny : y-1), z);
-        adjacents[10] = calc_index( x,                   (y == 0 ? Ny : y-1), z);
-        adjacents[11] = calc_index((x+1==Nx ?  0 : x+1), (y == 0 ? Ny : y-1), z);
-        adjacents[12] = calc_index((x  == 0 ? Nx : x-1), y,                   z);
-        adjacents[13] = calc_index( x,                   y,                   z);
-        adjacents[14] = calc_index((x+1==Nx ?  0 : x+1), y,                   z);
-        adjacents[15] = calc_index((x  == 0 ? Nx : x-1), (y+1==Ny ? 0 : y+1), z);
-        adjacents[16] = calc_index( x,                   (y+1==Ny ? 0 : y+1), z);
-        adjacents[17] = calc_index((x+1==Nx ?  0 : x+1), (y+1==Ny ? 0 : y+1), z);
-
-        adjacents[18] = calc_index((x  == 0 ? Nx : x-1), (y == 0 ? Ny : y-1), (z+1==Nz ? 0 : z+1));
-        adjacents[19] = calc_index( x,                   (y == 0 ? Ny : y-1), (z+1==Nz ? 0 : z+1));
-        adjacents[20] = calc_index((x+1==Nx ?  0 : x+1), (y == 0 ? Ny : y-1), (z+1==Nz ? 0 : z+1));
-        adjacents[21] = calc_index((x  == 0 ? Nx : x-1), y,                   (z+1==Nz ? 0 : z+1));
-        adjacents[22] = calc_index( x,                   y,                   (z+1==Nz ? 0 : z+1));
-        adjacents[23] = calc_index((x+1==Nx ?  0 : x+1), y,                   (z+1==Nz ? 0 : z+1));
-        adjacents[24] = calc_index((x  == 0 ? Nx : x-1), (y+1==Ny ? 0 : y+1), (z+1==Nz ? 0 : z+1));
-        adjacents[25] = calc_index( x,                   (y+1==Ny ? 0 : y+1), (z+1==Nz ? 0 : z+1));
-        adjacents[26] = calc_index((x+1==Nx ?  0 : x+1), (y+1==Ny ? 0 : y+1), (z+1==Nz ? 0 : z+1));
-
-        return adjacents;
-    }
 
     __host__ __device__
     std::size_t calc_index(
