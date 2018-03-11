0645db7a7a6aeabea42712da5f4258a55f78766c
ToruNiina <niina.toru.68u@gmail.com>
2018-03-11 19:27:02 +0900

    calculate force and energy

--- cuda/main.cu
+++ cuda/main.cu
@@ -8,6 +8,7 @@
 #include <thrust/functional.h>
 #include <thrust/for_each.h>
 #include <curand.h>
+#include <algorithm>
 #include <iterator>
 #include <iostream>
 #include <fstream>
@@ -16,10 +17,107 @@
 
 namespace lj
 {
-__device__ __host__ constexpr float sgm() noexcept {return 1.0f;}
-__device__ __host__ constexpr float eps() noexcept {return 1.0f;}
+__device__ __host__ constexpr float sgm()    noexcept {return 1.0f;}
+__device__ __host__ constexpr float eps()    noexcept {return 1.0f;}
 __device__ __host__ constexpr float cutoff() noexcept {return 2.5f;}
 __device__ __host__ constexpr float mergin() noexcept {return 0.1f;}
+
+struct force_calculator
+    : thrust::unary_function<float4, std::size_t>
+{
+    __host__
+    force_calculator(const float4* const ps, const std::size_t* const vl,
+                     const std::size_t* nneigh, const std::size_t stride_,
+                     const periodic_boundary b)
+        : positions(ps), verlet_list(vl), num_neighbors(nneigh),
+          inv_r_c(1.0 / (sgm() * cutoff())), stride(stride_), boundary(b)
+    {}
+
+    __host__ __device__
+    float4 operator()(const std::size_t i) const noexcept
+    {
+        float4 force = make_float4(0.0, 0.0, 0.0, 0.0);
+        const float4 pos1 = *(positions + i);
+
+        const std::size_t n_neigh = *(num_neighbors + i);
+        const std::size_t offset  = i * stride;
+
+        for(std::size_t idx=0; idx<n_neigh; ++idx)
+        {
+            assert(idx != std::numeric_limits<std::size_t>::max());
+
+            const std::size_t j = *(verlet_list + offset + idx);
+
+            const float4 pos2 = *(positions + j);
+            const float4 dpos = adjust_direction(pos2 - pos1, boundary);
+            const float  invr = rsqrtf(length_sq(dpos));
+            if(invr < inv_r_c) {continue;}
+
+            const float sgmr = sgm() * invr;
+            const float sr3  = sgmr * sgmr * sgmr;
+            const float sr6  = sr3 * sr3;
+            const float coef = 24 * eps() * sr6 * (1.0 - 2 * sr6) * invr;
+
+            force = force + dpos * coef;
+        }
+        return force;
+    }
+
+    const float4*      const positions;
+    const std::size_t* const verlet_list;
+    const std::size_t* const num_neighbors;
+    const float inv_r_c;
+    const std::size_t stride;
+    const periodic_boundary  boundary;
+};
+
+struct energy_calculator
+    : thrust::unary_function<float, std::size_t>
+{
+    __host__
+    energy_calculator(const float4* const ps, const std::size_t* const vl,
+                      const std::size_t* nneigh, const std::size_t stride_,
+                      const periodic_boundary b)
+        : positions(ps), verlet_list(vl), num_neighbors(nneigh),
+          inv_r_c(1.0 / (sgm() * cutoff())), stride(stride_), boundary(b)
+    {}
+
+    __device__ __host__
+    float operator()(const std::size_t i) const noexcept
+    {
+        float energy = 0.0;
+
+        const float4      pos1    = positions[i];
+        const std::size_t n_neigh = num_neighbors[i];
+        const std::size_t offset  = i * stride;
+
+        for(std::size_t idx=0; idx<n_neigh; ++idx)
+        {
+            assert(idx != std::numeric_limits<std::size_t>::max());
+            const std::size_t j = verlet_list[offset + idx];
+            assert(j < 64);
+            const float4 pos2 = positions[j];
+
+            const float4 dpos = adjust_direction(pos2 - pos1, boundary);
+            const float  invr = rsqrtf(length_sq(dpos));
+            if(invr < inv_r_c) {continue;}
+
+            const float  sgmr = sgm() * invr;
+            const float  sr3  = sgmr * sgmr * sgmr;
+            const float  sr6  = sr3 * sr3;
+            energy += 4 * eps() * sr6 * (sr6 - 1.0);
+        }
+
+        return energy / 2;
+    }
+
+    const float4*      const positions;
+    const std::size_t* const verlet_list;
+    const std::size_t* const num_neighbors;
+    const float inv_r_c;
+    const std::size_t stride;
+    const periodic_boundary boundary;
+};
 
 struct kinetic_energy_calculator
     : thrust::binary_function<float, float4, float>
@@ -28,10 +126,10 @@ struct kinetic_energy_calculator
     float operator()(float m, float4 v) const noexcept
     {
         return m * length_sq(v);
-    };
+    }
 };
 
-float kinetic_energy(const particle_container& ps)
+float calc_kinetic_energy(const particle_container& ps)
 {
     return thrust::inner_product(
             ps.device_masses.cbegin(),      ps.device_masses.cend(),
@@ -168,11 +266,164 @@ int main()
                 boltz_x.end(),   boltz_y.end(),   boltz_z.end())),
             ps.device_velocities.begin(), tuple_vector_converter());
     }
+    cudaDeviceSynchronize();
 
     ps.pull_device_particles();
 
     {
         std::ofstream traj("traj.xyz");
+        std::ofstream velo("velo.xyz");
+    }
+
+//     std::cerr << "kinetic energy = " << lj::calc_kinetic_energy(ps)
+//               << ", 3/2 NkBT = " << N * kB * T * 1.5 << std::endl;
+
+    //TODO: add potential
+    lj::grid grid(lj::sgm() * lj::cutoff(), lj::mergin(), boundary);
+    grid.assign(ps.device_positions);
+
+//    std::cerr << "assigned" << std::endl;
+//    for(std::size_t i=0; i<ps.device_positions.size(); ++i)
+//    {
+//        const std::size_t offset = i * grid.stride;
+//        std::cerr << "num_neighbors = " << grid.number_of_neighbors[i] << std::endl;
+//        std::cerr << '{';
+//        for(std::size_t n=0; n<grid.stride; ++n)
+//        {
+//            std::cerr << grid.verlet_list[n + offset] << ", ";
+//        }
+//        std::cerr << "}\n";
+//    }
+//    std::cerr << std::endl;
+
+    const lj::velocity_verlet_update_1 update1(dt, boundary);
+    const lj::velocity_verlet_update_2 update2(dt);
+
+    {
+        const lj::force_calculator  calc_f(ps.device_positions.data().get(),
+                                           grid.verlet_list.data().get(),
+                                           grid.number_of_neighbors.data().get(),
+                                           grid.stride,
+                                           boundary);
+        const lj::energy_calculator calc_e(ps.device_positions.data().get(),
+                                           grid.verlet_list.data().get(),
+                                           grid.number_of_neighbors.data().get(),
+                                           grid.stride,
+                                           boundary);
+
+        thrust::transform(thrust::counting_iterator<std::size_t>(0),
+            thrust::counting_iterator<std::size_t>(ps.device_positions.size()),
+            ps.device_forces.begin(), calc_f);
+    }
+
+    std::cout << "time\tkinetic\tpotential\ttotal\n";
+    for(std::size_t s=0; s < step; ++s)
+    {
+        /* if(s % 100 == 0) */
+        {
+            const lj::energy_calculator calc_e(
+                    ps.device_positions.data().get(),
+                    grid.verlet_list.data().get(),
+                    grid.number_of_neighbors.data().get(),
+                    grid.stride,
+                    boundary);
+
+            const float Ek = lj::calc_kinetic_energy(ps);
+            const float Ep = thrust::transform_reduce(
+                    thrust::counting_iterator<std::size_t>(0),
+                    thrust::counting_iterator<std::size_t>(ps.device_positions.size()),
+                    calc_e, 0.0, thrust::plus<float>());
+
+            std::cout << s * dt << '\t' << Ek << '\t' << Ep << '\t'
+                      << Ek + Ep << '\n';
+
+            ps.pull_device_particles();
+            std::ofstream traj("traj.xyz", std::ios_base::app | std::ios_base::out);
+
+            traj << ps.host_positions.size() << "\n\n";
+            for(auto iter = ps.host_positions.begin(), iend = ps.host_positions.end();
+                    iter != iend; ++iter)
+            {
+                const auto& v = *iter;
+                traj << "H      " << std::fixed << std::setprecision(5) << std::showpoint
+                     << std::setw(10) << std::right << v.x
+                     << std::setw(10) << std::right << v.y
+                     << std::setw(10) << std::right << v.z << '\n';
+            }
+
+            std::ofstream velo("velo.xyz", std::ios_base::app | std::ios_base::out);
+            thrust::host_vector<float4> host_velo(ps.device_velocities);
+            velo << ps.host_velocities.size() << "\n\n";
+            for(const auto& v : host_velo)
+            {
+                velo << "H      " << std::fixed << std::setprecision(5) << std::showpoint
+                     << std::setw(10) << std::right << v.x
+                     << std::setw(10) << std::right << v.y
+                     << std::setw(10) << std::right << v.z << '\n';
+            }
+        }
+
+        // get max(v(t)).
+        const float maxv = lj::length(*(thrust::max_element(
+                ps.device_velocities.cbegin(), ps.device_velocities.cend(),
+                lj::velocity_size_comparator())));
+
+        // p become p(t + dt), v become v(t + dt/2) here
+        thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(
+                ps.device_masses.begin(), ps.device_positions.begin(),
+                ps.device_velocities.begin(), ps.device_forces.begin())),
+            thrust::make_zip_iterator(thrust::make_tuple(
+                ps.device_masses.end(), ps.device_positions.end(),
+                ps.device_velocities.end(), ps.device_forces.end())),
+            update1);
+
+        cudaDeviceSynchronize();
+
+        // update cell-list
+        grid.update(ps.device_positions/* p(t+dt) */, 2 * maxv * dt /* max(v(t)) * dt */);
+
+        const lj::force_calculator  calc_f(ps.device_positions.data().get(),
+                                           grid.verlet_list.data().get(),
+                                           grid.number_of_neighbors.data().get(),
+                                           grid.stride,
+                                           boundary);
+
+        // calculate f(t+dt) using p(t+dt).
+        thrust::transform(thrust::counting_iterator<std::size_t>(0),
+                thrust::counting_iterator<std::size_t>(ps.device_positions.size()),
+                ps.device_forces.begin(), calc_f);
+
+        cudaDeviceSynchronize();
+
+        // v become v(t + dt), f become zero here
+        thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(
+                ps.device_masses.begin(),
+                ps.device_velocities.begin(), ps.device_forces.begin())),
+            thrust::make_zip_iterator(thrust::make_tuple(
+                ps.device_masses.end(),
+                ps.device_velocities.end(), ps.device_forces.end())),
+            update2);
+    }
+
+    {
+        const lj::energy_calculator calc_e(ps.device_positions.data().get(),
+                                           grid.verlet_list.data().get(),
+                                           grid.number_of_neighbors.data().get(),
+                                           grid.stride,
+                                           boundary);
+
+        const float Ek = lj::calc_kinetic_energy(ps);
+        const float Ep = thrust::transform_reduce(
+                thrust::counting_iterator<std::size_t>(0),
+                thrust::counting_iterator<std::size_t>(ps.device_positions.size()),
+                calc_e, 0.0, thrust::plus<float>());
+
+        std::cout << step * dt << '\t' << Ek << '\t' << Ep << '\t'
+                  << Ek + Ep << '\n';
+
+        ps.pull_device_particles();
+        std::ofstream traj("traj.xyz", std::ios_base::app | std::ios_base::out);
+
         traj << ps.host_positions.size() << "\n\n";
         for(auto iter = ps.host_positions.begin(), iend = ps.host_positions.end();
                 iter != iend; ++iter)
@@ -185,73 +436,6 @@ int main()
         }
     }
 
-    std::cerr << "kinetic energy = " << lj::kinetic_energy(ps)
-              << ", 3/2 NkBT = " << N * kB * T * 1.5 << std::endl;
-
-    //TODO: add potential
-    lj::grid grid(lj::sgm() * lj::cutoff(), lj::mergin(), boundary);
-//     std::cerr << grid.Nx << std::endl;
-//     std::cerr << grid.Ny << std::endl;
-//     std::cerr << grid.Nz << std::endl;
-
-    grid.assign(ps.device_positions);
-
-    std::cerr << "assigned" << std::endl;
-
-    for(std::size_t i=0; i<ps.device_positions.size(); ++i)
-    {
-        const std::size_t offset = i * grid.stride;
-        std::cerr << "num_neighbors = " << grid.number_of_neighbors[i] << std::endl;
-        std::cerr << '{';
-        for(std::size_t n=0; n<grid.stride; ++n)
-        {
-            std::cerr << grid.verlet_list[n + offset] << ", ";
-        }
-        std::cerr << "}\n";
-    }
-    std::cerr << std::endl;
-
-    const lj::velocity_verlet_update_1 update1(dt, boundary);
-    const lj::velocity_verlet_update_2 update2(dt);
-    for(std::size_t s=0; s < step; ++s)
-    {
-        if(s % 1000 == 0)
-        {
-            ps.pull_device_particles();
-
-            std::ofstream traj("traj.xyz",
-                    std::ios_base::app | std::ios_base::out);
-
-            traj << ps.host_positions.size() << "\n\n";
-            for(auto iter = ps.host_positions.begin(), iend = ps.host_positions.end();
-                    iter != iend; ++iter)
-            {
-                const auto& v = *iter;
-                traj << "H      " << std::fixed << std::setprecision(5) << std::showpoint
-                     << std::setw(10) << std::right << v.x
-                     << std::setw(10) << std::right << v.y
-                     << std::setw(10) << std::right << v.z << '\n';
-            }
-        }
-
-        thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(
-                ps.device_masses.begin(), ps.device_positions.begin(),
-                ps.device_velocities.begin(), ps.device_forces.begin())),
-            thrust::make_zip_iterator(thrust::make_tuple(
-                ps.device_masses.end(), ps.device_positions.end(),
-                ps.device_velocities.end(), ps.device_forces.end())),
-            update1);
-
-        // calc force here
-
-        thrust::for_each(thrust::make_zip_iterator(thrust::make_tuple(
-                ps.device_masses.begin(),
-                ps.device_velocities.begin(), ps.device_forces.begin())),
-            thrust::make_zip_iterator(thrust::make_tuple(
-                ps.device_masses.end(),
-                ps.device_velocities.end(), ps.device_forces.end())),
-            update2);
-    }
 
     return 0;
 }
