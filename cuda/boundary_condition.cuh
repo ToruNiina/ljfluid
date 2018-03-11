ae8c926e51a89be35d1437bb0d57855922239fa6
ToruNiina <niina.toru.68u@gmail.com>
2018-03-11 18:49:42 +0900

    add is_insode_of

--- cuda/boundary_condition.cuh
+++ cuda/boundary_condition.cuh
@@ -20,26 +20,38 @@ inline periodic_boundary make_boundary(const float4& lower, const float4& upper)
 }
 
 __device__ __host__
+inline bool is_inside_of(float4 pos, const periodic_boundary& b) noexcept
+{
+    if     (pos.x <  b.lower.x){return false;}
+    else if(pos.x >= b.upper.x){return false;}
+    if     (pos.y <  b.lower.y){return false;}
+    else if(pos.y >= b.upper.y){return false;}
+    if     (pos.z <  b.lower.z){return false;}
+    else if(pos.z >= b.upper.z){return false;}
+    return true;
+}
+
+__device__ __host__
 inline float4 adjust_direction(float4 pos, const periodic_boundary& b) noexcept
 {
-         if(pos.x < -b.half.x){pos.x += b.width.x;}
-    else if(pos.x >  b.half.x){pos.x -= b.width.x;}
-         if(pos.y < -b.half.y){pos.y += b.width.y;}
-    else if(pos.y >  b.half.y){pos.y -= b.width.y;}
-         if(pos.z < -b.half.z){pos.z += b.width.z;}
-    else if(pos.z >  b.half.z){pos.z -= b.width.z;}
+         if(pos.x <  -b.half.x){pos.x += b.width.x;}
+    else if(pos.x >=  b.half.x){pos.x -= b.width.x;}
+         if(pos.y <  -b.half.y){pos.y += b.width.y;}
+    else if(pos.y >=  b.half.y){pos.y -= b.width.y;}
+         if(pos.z <  -b.half.z){pos.z += b.width.z;}
+    else if(pos.z >=  b.half.z){pos.z -= b.width.z;}
     return pos;
 }
 
 __device__ __host__
 inline float4 adjust_position(float4 pos, const periodic_boundary& b) noexcept
 {
-         if(pos.x < b.lower.x){pos.x += b.width.x;}
-    else if(pos.x > b.upper.x){pos.x -= b.width.x;}
-         if(pos.y < b.lower.y){pos.y += b.width.y;}
-    else if(pos.y > b.upper.y){pos.y -= b.width.y;}
-         if(pos.z < b.lower.z){pos.z += b.width.z;}
-    else if(pos.z > b.upper.z){pos.z -= b.width.z;}
+         if(pos.x <  b.lower.x){pos.x += b.width.x;}
+    else if(pos.x >= b.upper.x){pos.x -= b.width.x;}
+         if(pos.y <  b.lower.y){pos.y += b.width.y;}
+    else if(pos.y >= b.upper.y){pos.y -= b.width.y;}
+         if(pos.z <  b.lower.z){pos.z += b.width.z;}
+    else if(pos.z >= b.upper.z){pos.z -= b.width.z;}
     return pos;
 }
 
